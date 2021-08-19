80cf3eba9ed84d73c31c8180c60ee147c44ca02f
JasonYoo1995 <jasonyoo950909@gmail.com>
2021-08-19 09:59:24 +0900

    nothing

--- src/main/java/com/konkuk/sample/SampleApplication.java
+++ src/main/java/com/konkuk/sample/SampleApplication.java
@@ -10,9 +10,3 @@ public class SampleApplication {
 		SpringApplication.run(SampleApplication.class, args);
 	}
 }
-
-/**
- 온전히 잘 작동되는 1번째 버전입니다.
- 추후 기능을 확장하거나 리팩터링, 안정화, 최적화할 것을 대비해
- 이렇게 안정적인 버전을 기록해놓습니다.
- */
